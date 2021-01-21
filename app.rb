65e83cc3fdb42c14b2c8d6aecefcc8f9d04a7709
steve <stevens714@yahoo.com>
2021-01-21 13:51:31 -0500

    Done.

--- app.rb
+++ app.rb
@@ -2,7 +2,11 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-    get '/name' do 
+  get '/' do
+    "Hello World"
+  end  
+  
+  get '/name' do 
         "My name is __"
       end
 
